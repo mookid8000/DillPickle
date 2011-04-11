6a3c671d4fa1e23e549c6c33af846e1862b221b5
Mogens Heller Grabe <mookid8000@gmail.com>
2011-04-11 11:10:44 +0200

    updated version to 0.8.0.7

--- src/DillPickle.Framework/Properties/AssemblyInfo.cs
+++ src/DillPickle.Framework/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ http://mookid.dk/oncode"
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.8.0.6")]
-[assembly: AssemblyFileVersion("0.8.0.6")]
+[assembly: AssemblyVersion("0.8.0.7")]
+[assembly: AssemblyFileVersion("0.8.0.7")]
