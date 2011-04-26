0ac2b2fc8c5ba623b1042987bf74bf4606ebbbb7
Mogens Heller Grabe <mookid8000@gmail.com>
2011-04-26 14:26:32 +0200

    incremented version no.

--- src/DillPickle.Framework/Properties/AssemblyInfo.cs
+++ src/DillPickle.Framework/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ http://mookid.dk/oncode"
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.8.1.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
