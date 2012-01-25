fd551550d8e958352d1a321405982bb450855745
Mogens Heller Grabe <mookid8000@gmail.com>
2012-01-25 10:44:30 +0100

    refactored build script and added ability to nupack itself

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 @echo off
 set msbuild=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
-%msbuild% scripts\build.proj /p:fx=NET35
-%msbuild% scripts\build.proj /p:fx=NET40
+%msbuild% scripts\build.proj
