610e71e11aac32962597768ee28fca31ec956aa6
Mogens Heller Grabe <mookid8000@gmail.com>
2012-01-25 10:07:29 +0100

    fixed msbuild path issue

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
-msbuild scripts\build.proj /p:fx=NET35
-msbuild scripts\build.proj /p:fx=NET40
+set msbuild=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+%msbuild% scripts\build.proj /p:fx=NET35
+%msbuild% scripts\build.proj /p:fx=NET40
